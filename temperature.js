cc18c22b5292abff91deca03377f1e65b3a70a97
Shashank Yadav <125982193+Shashank127Yadav@users.noreply.github.com>
2023-07-12 21:17:52 +0530

    Add files via upload

---
+++ temperature.js
@@ -0,0 +1,7 @@
+const calculateTemp = () => {
+    const numberTemp = document.getElementById('temp').ariaValueMax;
+    // console.log(numberTemp);
+    const tempSelected = document.getAnimations('temp_diff');
+     const valueTemp = temp_diff.optionss[tempSelected.selectedIndex];
+     console.log(valueTemp);
+    }
